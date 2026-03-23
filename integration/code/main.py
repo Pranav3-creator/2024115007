3628c0a7bcb714f9f0a96c8c92039d1cf698413d
Pranav <pranav.muvvala@gmail.com>
2026-03-23 22:31:35 +0530

    Added Main module

---
+++ integration/code/main.py
@@ -0,0 +1,14 @@
+from registration import Registration
+from crew import Crew
+from inventory import Inventory
+from race import Race
+from results import Results
+from mission import Mission
+from maintenance import Maintenance
+from leaderboard import Leaderboard
+
+def main():
+    print("StreetRace Manager System Running")
+
+if __name__ == "__main__":
+    main()
